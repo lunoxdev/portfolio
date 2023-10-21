f0e59b4de4be79ad71b4404166bacb2b0b9c4d2a
Lunox <lunox@Lunoxs-MacBook-Pro.local>
2023-10-20 22:00:48 -0300

    Version 0.1

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -23,14 +23,14 @@ const projects: Project[] = [
   {
     id: 1,
     name: "Casino",
-    img: "public/img/portfolio/casino.webp",
+    img: "../img/portfolio/casino.webp",
     url: "https://casino-games-app.netlify.app/",
     description: "A sophisticated casino platform for betting.",
   },
   {
     id: 2,
     name: "Afiliados",
-    img: "public/img/portfolio/afiliados.webp",
+    img: "../img/portfolio/afiliados.webp",
     url: "https://afiliados.netlify.app/",
     description:
       "An affiliate LATAM website for online sports betting platforms.",
@@ -38,7 +38,7 @@ const projects: Project[] = [
   {
     id: 3,
     name: "Rick & Morty",
-    img: "public/img/portfolio/rickandmorty.webp",
+    img: "../img/portfolio/rickandmorty.webp",
     url: "https://rick-morty-adventures.netlify.app/",
     description:
       "Embark on a captivating journey through the universe of this beloved animated series as you explore character profiles seamlessly connected to an API.",
@@ -49,19 +49,19 @@ const social: Social[] = [
   {
     id: 1,
     nombre: "LinkedIn",
-    logo: "public/img/social/linkedin.svg",
+    logo: "../img/social/linkedin.svg",
     url: "https://www.linkedin.com/in/moises-obando/",
   },
   {
     id: 2,
     nombre: "GitHub",
-    logo: "public/img/social/github.svg",
+    logo: "../img/social/github.svg",
     url: "https://github.com/Lunox-code",
   },
   {
     id: 3,
     nombre: "Instagram",
-    logo: "public/img/social/ig.svg",
+    logo: "../img/social/ig.svg",
     url: "https://www.instagram.com/lunox.code/",
   },
 ];
@@ -70,32 +70,32 @@ const skills: Skill[] = [
   {
     id: 1,
     nombre: "React JS",
-    logo: "public/img/skills/react-2.svg",
+    logo: "../img/skills/react-2.svg",
   },
   {
     id: 2,
     nombre: "JavaScript",
-    logo: "public/img/skills/javascript.svg",
+    logo: "../img/skills/javascript.svg",
   },
   {
     id: 3,
     nombre: "HTML 5",
-    logo: "public/img/skills/html5.svg",
+    logo: "../img/skills/html5.svg",
   },
   {
     id: 4,
     nombre: "CSS 3",
-    logo: "public/img/skills/css-3.svg",
+    logo: "../img/skills/css-3.svg",
   },
   {
     id: 5,
     nombre: "Figma",
-    logo: "public/img/skills/figma.svg",
+    logo: "../img/skills/figma.svg",
   },
   {
     id: 6,
     nombre: "Tailwind CSS",
-    logo: "public/img/skills/tailwind-css.svg",
+    logo: "../img/skills/tailwind-css.svg",
   },
 ];
 
